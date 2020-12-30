6407db258333cda417e7ea546ac2e9b32049600f
bamamoudou <mamoudouba8787@gmail.com>
2020-12-30 17:45:54 +0100

    Init project

---
+++ src/main/java/com/paymybuddy/moneytranfer/MoneytranferApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.moneytranfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytranferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytranferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/moneytranfer/MoneytranferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.moneytranfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytranferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
