ad6ef22f99df8cb159c0c7a9a557ceb99d1f33b3
bamamoudou <mamoudouba8787@gmail.com>
2021-01-01 16:18:35 +0100

    init project

--- src/main/java/com/paymybuddy/moneytranfer/MoneytranferApplication.java
+++ src/main/java/com/paymybuddy/moneytranfer/MoneytranferApplication.java
@@ -2,8 +2,10 @@ package com.paymybuddy.moneytranfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class MoneytranferApplication {
 
 	public static void main(String[] args) {
