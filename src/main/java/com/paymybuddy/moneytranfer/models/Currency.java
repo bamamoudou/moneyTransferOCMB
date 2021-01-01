a69ba1673d15573725618892b1fd0204812c2985
bamamoudou <mamoudouba8787@gmail.com>
2021-01-01 19:36:31 +0100

    Implement currency model via unit tests

--- src/main/java/com/paymybuddy/moneytranfer/models/Currency.java
+++ src/main/java/com/paymybuddy/moneytranfer/models/Currency.java
@@ -2,4 +2,51 @@ package com.paymybuddy.moneytranfer.models;
 
 public class Currency {
 
+	private int id;
+
+	private String label;
+
+	private String description;
+
+	public Currency(int id, String label, String description) {
+		super();
+		this.id = id;
+		this.label = label;
+		this.description = description;
+	}
+
+	public Currency(String label, String description) {
+		super();
+		this.label = label;
+		this.description = description;
+	}
+
+	public Currency() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
---
+++ src/test/java/com/paymybuddy/moneytranfer/modelsTest/CurrencyTest.java
@@ -0,0 +1,41 @@
+package com.paymybuddy.moneytranfer.modelsTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.paymybuddy.moneytranfer.models.Currency;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CurrencyTest {
+
+	private Currency currency;
+
+	@BeforeEach
+	public void initTest() {
+		currency = new Currency(1, "£", "Euro");
+
+	}
+
+	@Test
+	public void gettersTest() {
+		assertThat(currency.getId()).isEqualTo(1);
+		assertThat(currency.getLabel()).isEqualTo("£");
+		assertThat(currency.getDescription()).isEqualTo("Euro");
+	}
+
+	@Test
+	public void settersTest() {
+		currency.setId(2);
+		currency.setLabel("$");
+		currency.setDescription("Dollar US");
+
+		assertThat(currency.getId()).isEqualTo(2);
+		assertThat(currency.getLabel()).isEqualTo("$");
+		assertThat(currency.getDescription()).isEqualTo("Dollar US");
+	}
+
+}
