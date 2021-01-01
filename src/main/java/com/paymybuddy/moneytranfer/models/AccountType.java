cf02a07ce16ce0f05e96d51cc241b8f3fcfc2d09
bamamoudou <mamoudouba8787@gmail.com>
2021-01-01 16:17:07 +0100

    Implement account and account type model via unit tests

--- src/main/java/com/paymybuddy/moneytranfer/models/AccountType.java
+++ src/main/java/com/paymybuddy/moneytranfer/models/AccountType.java
@@ -1,18 +1,11 @@
 package com.paymybuddy.moneytranfer.models;
 
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-@Entity
-@Table(name = "account_type", catalog = "fund_transfer")
-@EntityListeners(AuditingEntityListener.class)
+//@Entity
+//@Table(name = "account_type", catalog = "fund_transfer")
+//@EntityListeners(AuditingEntityListener.class)
 public class AccountType {
 
-	@Id
+	// @Id
 	private int id;
 	private String accountType;
 
