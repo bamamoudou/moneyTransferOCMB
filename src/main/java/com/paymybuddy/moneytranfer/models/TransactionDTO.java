9cdd34e1841f2f670c9cb00783785300c1146c55
bamamoudou <mamoudouba8787@gmail.com>
2021-01-04 19:58:14 +0100

    Implement dto model for transaction

---
+++ src/main/java/com/paymybuddy/moneytranfer/models/TransactionDTO.java
@@ -0,0 +1,44 @@
+package com.paymybuddy.moneytranfer.models;
+
+public class TransactionDTO {
+
+    private String toUserEmail;
+    private String amount;
+    private String description;
+
+    public TransactionDTO() {
+        this.toUserEmail = "";
+        this.amount = "0.0";
+        this.description = "";
+    }
+
+    public TransactionDTO(String toUserEmail, String amount, String description) {
+        this.toUserEmail = toUserEmail;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public String getToUserEmail() {
+        return toUserEmail;
+    }
+
+    public void setToUserEmail(String toUserEmail) {
+        this.toUserEmail = toUserEmail;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
