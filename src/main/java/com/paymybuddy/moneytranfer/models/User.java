b6eb6c77a0e931422701e977c9d09e2ee0438ef4
bamamoudou <mamoudouba8787@gmail.com>
2021-01-04 19:01:16 +0100

    Implement user model via unit tests

--- src/main/java/com/paymybuddy/moneytranfer/models/User.java
+++ src/main/java/com/paymybuddy/moneytranfer/models/User.java
@@ -1,5 +1,135 @@
 package com.paymybuddy.moneytranfer.models;
 
+import java.util.Date;
+import java.util.List;
+
 public class User {
+
+	private int id;
+
+	private Role role;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	private Date createdAt;
+
+	private Date updatedAt;
+
+	private boolean isActive;
+
+	private List<Connection> connections;
+
+	private Account account;
+
+	public User(int id, Role role, String name, String email, String password, Date createdAt, Date updatedAt,
+			boolean isActive, List<Connection> connections, Account account) {
+		super();
+		this.id = id;
+		this.role = role;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.isActive = isActive;
+		this.connections = connections;
+		this.account = account;
+	}
+
+	public User(Role role, String name, String email, String password) {
+		super();
+		this.role = role;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public User() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public List<Connection> getConnections() {
+		return connections;
+	}
+
+	public void setConnections(List<Connection> connections) {
+		this.connections = connections;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 
 }
---
+++ src/test/java/com/paymybuddy/moneytranfer/modelsTest/UserTest.java
@@ -0,0 +1,97 @@
+package com.paymybuddy.moneytranfer.modelsTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.paymybuddy.moneytranfer.models.Account;
+import com.paymybuddy.moneytranfer.models.Connection;
+import com.paymybuddy.moneytranfer.models.Role;
+import com.paymybuddy.moneytranfer.models.User;
+
+@ExtendWith(MockitoExtension.class)
+public class UserTest {
+
+	private User user;
+
+	@Mock
+	private static Account account;
+
+	@Mock
+	private static Account accountSet;
+
+	@Mock
+	private static List<Connection> connections;
+
+	@Mock
+	private static List<Connection> connectionsSet;
+
+	@Mock
+	private Role role;
+
+	@Mock
+	private Role roleSet;
+
+	private Date createdAt;
+
+	private Date createdAtSet;
+
+	private Date updatedAt;
+
+	private Date updatedAtSet;
+
+	@BeforeEach
+	public void initTest() {
+		user = new User(1, role, "Doro", "doro@doro.fr", "mdp", createdAt, updatedAt, true, connections, account);
+
+	}
+
+	@Test
+	public void gettersTest() {
+		assertThat(user.getId()).isEqualTo(1);
+		assertThat(user.getRole()).isEqualTo(role);
+		assertThat(user.getName()).isEqualTo("Doro");
+		assertThat(user.getEmail()).isEqualTo("doro@doro.fr");
+		assertThat(user.getPassword()).isEqualTo("mdp");
+		assertThat(user.getCreatedAt()).isEqualTo(createdAt);
+		assertThat(user.getUpdatedAt()).isEqualTo(updatedAt);
+		assertThat(user.isActive()).isEqualTo(true);
+		assertThat(user.getConnections()).isEqualTo(connections);
+		assertThat(user.getAccount()).isEqualTo(account);
+
+	}
+
+	@Test
+	public void settersTest() {
+		user.setId(2);
+		user.setRole(roleSet);
+		user.setName("Mamoudou");
+		user.setEmail("ma@ma.fr");
+		user.setPassword("pwd");
+		user.setCreatedAt(createdAtSet);
+		user.setUpdatedAt(updatedAtSet);
+		user.setActive(false);
+		user.setConnections(connectionsSet);
+		user.setAccount(accountSet);
+
+		assertThat(user.getId()).isEqualTo(2);
+		assertThat(user.getRole()).isEqualTo(roleSet);
+		assertThat(user.getName()).isEqualTo("Mamoudou");
+		assertThat(user.getEmail()).isEqualTo("ma@ma.fr");
+		assertThat(user.getPassword()).isEqualTo("pwd");
+		assertThat(user.getCreatedAt()).isEqualTo(createdAtSet);
+		assertThat(user.getUpdatedAt()).isEqualTo(updatedAtSet);
+		assertThat(user.isActive()).isEqualTo(false);
+		assertThat(user.getConnections()).isEqualTo(connectionsSet);
+		assertThat(user.getAccount()).isEqualTo(accountSet);
+
+	}
+
+}
