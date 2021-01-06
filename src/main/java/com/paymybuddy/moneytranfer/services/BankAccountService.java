833e1f35067bb799db6ef571e92b435d7bd47af6
bamamoudou <mamoudouba8787@gmail.com>
2021-01-06 20:17:35 +0100

    Implement bank account service

---
+++ src/main/java/com/paymybuddy/moneytranfer/services/BankAccountService.java
@@ -0,0 +1,14 @@
+package com.paymybuddy.moneytranfer.services;
+
+import com.paymybuddy.moneytranfer.models.Account;
+import com.paymybuddy.moneytranfer.models.BankAccount;
+import com.paymybuddy.moneytranfer.models.User;
+
+public interface BankAccountService {
+
+	public BankAccount findBankAccountByAccount(Account account);
+
+	public BankAccount createBankAccount(User user, String bankAccountNumber);
+
+
+}
