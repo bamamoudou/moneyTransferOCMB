9a83ae5e8ab13c59aeeb81d271ffb82ceee079dd
bamamoudou <mamoudouba8787@gmail.com>
2021-01-07 20:41:58 +0100

    Implement currency service

---
+++ src/main/java/com/paymybuddy/moneytranfer/services/CurrencyService.java
@@ -0,0 +1,9 @@
+package com.paymybuddy.moneytranfer.services;
+
+import com.paymybuddy.moneytranfer.models.Currency;
+
+public interface CurrencyService {
+	
+	public Currency findCurrencyByCurrencyLabel(String currencyLabel);
+
+}
