784791bcd8b7f2d2b59a3b28c84c0c455078fe74
bamamoudou <mamoudouba8787@gmail.com>
2021-01-08 18:22:50 +0100

    Find transaction by account and transaction type

--- src/main/java/com/paymybuddy/moneytranfer/servicesImpl/TransactionServiceImpl.java
+++ src/main/java/com/paymybuddy/moneytranfer/servicesImpl/TransactionServiceImpl.java
@@ -1,41 +1,70 @@
 package com.paymybuddy.moneytranfer.servicesImpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.paymybuddy.moneytranfer.models.Account;
 import com.paymybuddy.moneytranfer.models.Transaction;
 import com.paymybuddy.moneytranfer.models.User;
+import com.paymybuddy.moneytranfer.repositories.TransactionRepository;
+import com.paymybuddy.moneytranfer.repositories.TransactionTypeRepository;
+import com.paymybuddy.moneytranfer.services.AccountService;
+import com.paymybuddy.moneytranfer.services.BankAccountService;
+import com.paymybuddy.moneytranfer.services.CurrencyService;
 import com.paymybuddy.moneytranfer.services.TransactionService;
 
 public class TransactionServiceImpl implements TransactionService {
+	private TransactionRepository transactionRepository;
+	private TransactionTypeRepository transactionTypeRepository;
+	private AccountService accountService;
+	private CurrencyService currencyService;
+	private BankAccountService bankAccountService;
+	/**
+	 * 0.5% transaction fee to be collected from sending user for every normal
+	 * transaction (from friend to friend)
+	 */
+	private Double transactionFee = new Double(0.005);
 
-	@Override
-	public List<Transaction> findTransactionListByTransactionType(String transactionType) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	public TransactionServiceImpl(TransactionRepository transactionRepository,
+			TransactionTypeRepository transactionTypeRepository, AccountService accountService,
+			CurrencyService currencyService, BankAccountService bankAccountService) {
+		this.transactionRepository = transactionRepository;
+		this.transactionTypeRepository = transactionTypeRepository;
+		this.accountService = accountService;
+		this.currencyService = currencyService;
+		this.bankAccountService = bankAccountService;
 	}
 
 	@Override
-	public List<Transaction> findTransactionListByAccount(Account account) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Transaction> findTransactionsByTransactionType(String transactionType) {
+		List<Transaction> transactions = transactionRepository.findAll().stream()
+				.filter(t -> t.getTransactionType().getType().equals(transactionType)).collect(Collectors.toList());
+		return transactions;
 	}
 
 	@Override
-	public void createTransactionByTransferToFriend(User sendingUser, String receivingUserEmail, String description,
+	public List<Transaction> findTransactionsByAccount(Account account) {
+		return transactionRepository.findTransactionListByAccount(account);
+	}
+
+	@Override
+	public void createTransactionByPayMyBuddy(User sendingUser, String receivingUserEmail, String description,
 			String amount) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void createTransactionByTransferToBank(User sendingUser) {
+	public void createTransactionByTransferToBankAccount(User sendingUser) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void createTransactionByAddMoney(User sendingUser, String amount) {
+	public void createTransactionByCreditMyAccount(User sendingUser, String amount) {
 		// TODO Auto-generated method stub
 
 	}
--- src/test/java/com/paymybuddy/moneytranfer/servicesTest/TransactionServiceTest.java
+++ src/test/java/com/paymybuddy/moneytranfer/servicesTest/TransactionServiceTest.java
@@ -1,5 +1,97 @@
 package com.paymybuddy.moneytranfer.servicesTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.paymybuddy.moneytranfer.models.Account;
+import com.paymybuddy.moneytranfer.models.Role;
+import com.paymybuddy.moneytranfer.models.Transaction;
+import com.paymybuddy.moneytranfer.models.TransactionType;
+import com.paymybuddy.moneytranfer.models.User;
+import com.paymybuddy.moneytranfer.repositories.TransactionRepository;
+import com.paymybuddy.moneytranfer.repositories.TransactionTypeRepository;
+import com.paymybuddy.moneytranfer.services.AccountService;
+import com.paymybuddy.moneytranfer.services.BankAccountService;
+import com.paymybuddy.moneytranfer.services.CurrencyService;
+import com.paymybuddy.moneytranfer.servicesImpl.TransactionServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
+	@Mock
+	private TransactionRepository transactionRepository;
+	@Mock
+	private TransactionTypeRepository transactionTypeRepository;
+	@Mock
+	private AccountService accountService;
+	@Mock
+	private CurrencyService currencyService;
+	@Mock
+	private BankAccountService bankAccountService;
+	@InjectMocks
+	private TransactionServiceImpl transactionServiceImpl;
+	Role role;
+	User user1;
+	User user2;
+	Account account1;
+	Account account2;
+
+	@BeforeEach
+	public void initTest() {
+		transactionServiceImpl = new TransactionServiceImpl(transactionRepository, transactionTypeRepository,
+				accountService, currencyService, bankAccountService);
+		role = new Role("User");
+		user1 = new User(role, "user_1", "user1@test.fr", "test123");
+		user2 = new User(role, "user_2", "user2@test.fr", "test123");
+
+		account1 = new Account();
+		account1.setId(1);
+		account1.setUser(user1);
+		account1.setBalance(new Double(0.0));
+		user1.setAccount(account1);
+
+		account2 = new Account();
+		account2.setId(2);
+		account2.setUser(user2);
+		account2.setBalance(new Double(0.0));
+		user1.setAccount(account2);
+	}
+
+	@Test
+	public void findTransactionsByAccountIfAccountExistsAndTransactionListReturned() {
+		// arrange
+		Transaction transaction = new Transaction(account1, account2.getId(), new Double(10.0));
+
+		when(transactionRepository.findTransactionListByAccount(account1)).thenReturn(Arrays.asList(transaction));
+		// act
+		List<Transaction> result = transactionServiceImpl.findTransactionsByAccount(account1);
+		// assert
+		assertThat(result.size()).isEqualTo(1);
+	}
+
+	@Test
+	public void findTransactionsByTransactionTypeIfTransactionTypeExistsAndTransactionListReturned() {
+		// arrange
+		Transaction transaction = new Transaction(account1, account2.getId(), new Double(10.0));
+		TransactionType transactionType = new TransactionType("Normal");
+		transaction.setTransactionType(transactionType);
+
+		when(transactionRepository.findAll()).thenReturn(Arrays.asList(transaction));
+
+		// act
+		List<Transaction> result = transactionServiceImpl.findTransactionsByTransactionType("Normal");
+
+		// assert
+		assertThat(result.size()).isEqualTo(1);
+	}
 
 }
